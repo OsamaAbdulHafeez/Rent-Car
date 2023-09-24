011394950822b3744911db0a90d6661273529790
OsamaAbdulHafeez <osamahafeez0987@gmail.com>
2023-09-24 18:24:39 +0500

    counterComplete

---
+++ app.js
@@ -0,0 +1,11 @@
+// var Toggle = document.getElementById("toggle")
+// Toggle.style.maxHeight = '0px'
+// function ToggleChange(){
+//     if(Toggle.style.maxHeight == "0px"){
+//         Toggle.style.height = "fit-content"
+//     }
+//     else {
+//         Toggle.style.maxHeight = "0px"
+//     }
+//     console.log("Osama")
+// }
